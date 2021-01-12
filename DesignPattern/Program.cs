116391cd3f2d60d487a7c5e524dc54560e210381
Ibrahim Radi <60944957+i-radi@users.noreply.github.com>
2021-01-13 01:45:44 +0200

    Singleton Pattern

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             #region Singletol
+            //Same Instance
+            SingletonClass s1= SingletonClass.GetInstance();
+            SingletonClass s2= SingletonClass.GetInstance();
+            #endregion
+            
+            
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ DesignPattern/SingletonClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    sealed class SingletonClass
+    {
+        private static SingletonClass _instance = null;
+
+        private SingletonClass()
+        {
+            Console.WriteLine("This's Singleton Class");   
+        }
+
+        public static SingletonClass GetInstance() => _instance ?? new SingletonClass();
+
+    }
+}
